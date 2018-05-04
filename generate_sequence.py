bdfd3b34ebb9566b0b9ee03b6444155361f940d9
Martin <Martin.Rueckert@physik.uni-wuerzburg.de>
2018-05-04 17:58:19 +0200

    fix time scale in generate_sequence.py

--- generate_sequence.py
+++ generate_sequence.py
@@ -54,15 +54,17 @@ baudrate         = 9600 # USBFS component ignores this parameter
 time_out         = 1 # [s]; this script should retrieve the 60 kB data in << 1s
 
 # 0.2) channel settings
-num_channels = 1            # number of channels
-nsamples_ramp_up = 4     # number of steps for the ramp up sequence
-nsamples_sequence = 24    # number of steps for the actual sequence
-nsamples_ramp_down = 4   # number of steps for the ramp up sequence
+num_channels = 4            # number of channels
+nsamples_ramp_up = 3750     # number of steps for the ramp up sequence
+nsamples_sequence = 3750    # number of steps for the actual sequence
+nsamples_ramp_down = 3750   # number of steps for the ramp up sequence
 nsamples_total = nsamples_ramp_up + nsamples_sequence + nsamples_ramp_down  # total number of steps
 
 max_value = 180     
 values = np.zeros((num_channels, nsamples_total),  dtype=np.uint8) # 2D array storing the sequence
 
+dac_sampling_rate = 250e3
+frequency_scale = 1/dac_sampling_rate
 
 # 0.3) package settings
 len_header = 8
@@ -72,36 +74,38 @@ len_data = 32
 
 
 """ generates sequence """
-f = [0.01,0.01,700,1600,1600]         # frequency in Hz
-f_mod = [0.0,0,0,0]      # in Hz
-phi = [40,90,0,0]         # in degree
-phi_mod = [90,90,0,0]     # in degree
-amp = [0.5 * max_value, 0.5 * max_value, 0.5 * max_value, 0.5 * max_value]        
-off = [0.5 * max_value, 0.5 * max_value, 0.5 * max_value, 0.5 * max_value]
+f = frequency_scale*np.asarray([700,700,16000,17000])         # frequency in Hz
+f_mod = frequency_scale*np.asarray([0.0,0,0,0])      # in Hz
+phi = np.asarray([0,90,0,0])         # in degree
+phi_mod = np.asarray([90,90,90,90])     # in degree
+amp = np.asarray([0.5 * max_value, 0.5 * max_value, 0.5 * max_value, 0.5 * max_value])        
+off = np.asarray([0.5 * max_value, 0.5 * max_value, 0.5 * max_value, 0.5 * max_value])
 
 
 for channel in range(num_channels):
     # ramp up
-    for t in range(nsamples_ramp_up):
+    for t in np.arange(nsamples_ramp_up):
         values[channel][t] = 1.0 / nsamples_ramp_up * t \
         * amp[channel] * np.sin(2 * np.pi * f[channel] * t + phi[channel]*np.pi/180) \
         * np.sin(2 * np.pi * f_mod[channel] * t + phi_mod[channel]*np.pi/180) + off[channel]
     # actual sequence
-    for t in range(nsamples_sequence):
-        values[channel][t+nsamples_ramp_up] = \
+    for t in (np.arange(nsamples_sequence)+nsamples_ramp_up):
+        values[channel][t] = \
         amp[channel] * np.sin(2 * np.pi * f[channel] * t + phi[channel]*np.pi/180) \
         * np.sin(2 * np.pi * f_mod[channel] * t + phi_mod[channel]*np.pi/180) + off[channel]
     # ramp down
-    for t in range(nsamples_ramp_down):
-        values[channel][t+nsamples_ramp_up + nsamples_sequence] = (1.0 - t / nsamples_ramp_down) \
+    for t in (np.arange(nsamples_ramp_down)+nsamples_ramp_up+nsamples_sequence):
+        values[channel][t] = (1.0 - (t-nsamples_ramp_up-nsamples_sequence) / nsamples_ramp_down) \
         * amp[channel] * np.sin(2 * np.pi * f[channel] * t + phi[channel]*np.pi/180) \
         * np.sin(2 * np.pi * f_mod[channel] * t + phi_mod[channel]*np.pi/180) + off[channel]
 
 
-# Plots the generated sequence
 
+# Plots the generated sequence
 for channel in range(num_channels):
-    plt.plot(range(nsamples_total), values[channel])
+    plt.plot(np.arange(nsamples_total)/dac_sampling_rate, values[channel])
+    plt.xlabel('time [s]')
+    plt.ylabel('dac value (uint8)')
 plt.show()
 
 
@@ -137,13 +141,13 @@ for channel in range(num_channels):
         if data.size != 32:
             data = np.append(data, np.zeros(32-data.size,dtype=np.uint8))
             
-        data_bytes = bytes(data)
+        #data_bytes = bytes(data)
 
-        ser.write(header_bytes + data_bytes)
+        #ser.write(header_bytes + data_bytes)
 
 ser.close()
-
-
-
+#
+#
+#
 
 
